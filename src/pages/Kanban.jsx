e813433f0a99aa7607a19547ae249edcd3dd7660
Kiggz <nico.peignot@outlook.fr>
2023-07-03 15:42:04 +0200

    base dom

---
+++ src/pages/Kanban.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+
+const Kanban = () => {
+  return(
+    <>
+    <Sidebar/>
+    <div>
+      <Header/>
+      <div></div>
+    </div>
+    </>
+  )
+}
+
+export default Kanban
